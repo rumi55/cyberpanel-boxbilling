467d3b4b76e25fdc8cc9dfd0e3562279939aa97f
rumi55 <rumi55@users.noreply.github.com>
2018-08-06 17:15:52 +0200

    Update Cyberpanel.php

--- Cyberpanel.php
+++ Cyberpanel.php
@@ -1,5 +1,34 @@
 <?php
 
 class Server_Manager_CyberPanel extends Server_Manager {
-
+  
+  public function init() {
+    
+  }
+  
+      public static function getForm()
+    {
+        return array(
+            'label'     =>  'Cyber Panel',
+        );
+    }
+    public function getLoginUrl()
+    {
+        $host     = $this->_config['host'];
+        return 'http://'.$host.':8090';
+    }
+    public function getResellerLoginUrl()
+    {
+        $host     = $this->_config['host'];
+        return 'http://'.$host.':8090';
+    }
+    public function testConnection()
+    {
+        return TRUE;
+    }
+    public function synchronizeAccount(Server_Account $a)
+    {
+        $this->getLog()->info('Synchronizing account with server '.$a->getUsername());
+        return $a;
+    }
 }
