18839e370de1964b163d3627b57fa38c9bc20589
rumi55 <rumi55@users.noreply.github.com>
2018-08-06 17:18:11 +0200

    Update Cyberpanel.php

--- Cyberpanel.php
+++ Cyberpanel.php
@@ -24,8 +24,44 @@ class Server_Manager_CyberPanel extends Server_Manager {
     }
     public function testConnection()
     {
-        return TRUE;
+        
+      try {
+        $adminUser = $params["serverusername"];
+        $adminPass = $params["serverpassword"];
+
+        $api = new CyberApi();
+        $test_conn = $api->verify_connection($params, $adminUser, $adminPass);
+
+        // Checking for errors
+        $errorMsg = '';
+        if (!$test_conn["verifyConn"]){
+        	$errorMsg =  $test_conn["error_message"];
+        	$success = false;
+        }
+        else {
+        	$success = true;
+        	$errorMsg = '';
+        }
+        } catch (Exception $e) {
+        // Record the error in WHMCS's module log.
+        logModuleCall(
+            'cyberpanel',
+            __FUNCTION__,
+            $params,
+            $e->getMessage(),
+            $e->getTraceAsString()
+        );
+
+        $success = false;
+        $errorMsg = $e->getMessage();
     }
+
+    return array(
+        'success' => $success,
+        'error' => $errorMsg,
+    );
+    }
+  
     public function synchronizeAccount(Server_Account $a)
     {
         $this->getLog()->info('Synchronizing account with server '.$a->getUsername());
